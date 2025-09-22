270cecf185aa3ca8da4cdffdd6e4f365987fd514
Markus Zehnder <markus.z@unfoldedcircle.com>
2025-09-22 22:49:10 +0200

    refactor: don't start wifi setup if network_wifi_connect failed (#38)
    If `connectActiveSsid()` in state WIFI_CONNECTING_STATE cannot initiate a
    WiFi connection with `network_wifi_connect` , try one more time and reboot
    instead of entering  WiFi setup mode.
    The WiFi credentials are available at this state and there's no need to start
    WiFi provisioning.
    
    Related to unfoldedcircle/feature-and-bug-tracker#612

--- components/network/src/NetworkSmBase.cpp
+++ components/network/src/NetworkSmBase.cpp
@@ -243,10 +243,15 @@ void NetworkBase::connectActiveSsid() {
             ESP_LOGI(TAG, "Ethernet connection is found.  Try to fallback there");
             trigger_eth_fallback_event();
         } else {
-            // start WiFi provisioning
-            sta_duration_ms_ = CONFIG_NETWORK_MANAGER_STA_POLLING_MIN * 1000;
-            ESP_LOGI(TAG, "No ethernet and no WiFi configured. Starting WiFi provisioning");
-            trigger_configure_wifi_event();
+            // This should not happen! SSID & pwd were checked before and are available,
+            // something must have gone wrong in network_wifi_connect.
+            // Likely causes https://github.com/unfoldedcircle/feature-and-bug-tracker/issues/612
+            ESP_LOGE(TAG, "Failed to initiate WiFi connection. Trying max 2 more times...");
+
+            // Retry again, dock is restarted when reaching max retry
+            if (retries_ < CONFIG_NETWORK_MANAGER_MAX_RETRY - 1) {
+                retries_ = CONFIG_NETWORK_MANAGER_MAX_RETRY - 1;
+            }
         }
     }
 }
